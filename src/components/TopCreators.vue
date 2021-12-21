5a37e7313257192cc5333633309c86afd33114e6
Marko Marković <you@example.com>
2021-12-21 15:26:17 +0100

    added top creators vue

---
+++ src/components/TopCreators.vue
@@ -0,0 +1,37 @@
+
+<template></template>   
+
+something like this in future
+       ||
+       \/
+       
+<!-- // <div id="app">
+//   <ul>
+//     <li v-for="creator in creators">
+//     <div>
+//        <img :src=creator.image >
+//        <p>{{ creator.name }}</p>
+//        <span>{{ creator.category }}</span>
+//     </div>
+//     </li>
+//   </ul>
+// </div> -->
+ <!-- </template> -->
+<!-- <script> -->
+<!-- myObject = new Vue({
+  el: '#app',
+  data: {
+    creators: [
+      { name: 'Learn JavaScript',
+      category: 'sport',
+      image: 'https://miami.momcollective.com/wp-content/uploads/2019/08/facial-expression-fashion-hairstyle-1674752-e1566182464204-600x523.jpg' },
+      { name: 'Learn Vue.js',
+      category: 'beauty',
+      image: 'https://yt3.ggpht.com/ytc/AKedOLSl1xdUqFKVK9Fxl9RW8o2FWpY1IXJVswhlv1yVLQ=s900-c-k-c0x00ffffff-no-rj' },
+      { name: 'Build Something Awesome',
+      category: 'art',
+      image: 'https://miami.momcollective.com/wp-content/uploads/2019/08/facial-expression-fashion-hairstyle-1674752-e1566182464204-600x523.jpg' }
+    ]
+  }
+}) -->
+<!-- </script> -->
